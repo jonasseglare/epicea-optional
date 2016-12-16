74efa1d48a545c4f3ab1200e97cecbe8a65d9eda
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-12-16 22:05:17 +0100

    bra va

--- test/epicea_optional/core_test.clj
+++ test/epicea_optional/core_test.clj
@@ -5,5 +5,5 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 9 (either 9)))
-    (is (= 4 (either (optionally false 3)
-                     4)))))
+    (is (= 4 (either (optionally false 3) 4)))
+    (is (= 3 (either (optionally true 3) 4)))))
